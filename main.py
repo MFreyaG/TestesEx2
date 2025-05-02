ae9ae3c54a06a1a000d8919c4cf9186cff5b2da3
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2025-05-01 22:01:33 -0300

    3. Classe TestResult

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from test_case import TestCase
+from test_result import TestResult
 
 class MyTest(TestCase):
     def set_up(self):
-        print('set_up')
+        pass
     
     def tear_down(self):
-        print('tear_down')
+        pass
         
     def test_a(self):
-        print('test_a')
+        pass
 
     def test_b(self):
-        print('test_b')
+        pass
 
     def test_c(self):
-        print('test_c')
+        pass
+
+result = TestResult()
 
 test = MyTest('test_a')
-test.run()
+test.run(result)
 
 test = MyTest('test_b')
-test.run()
+test.run(result)
 
 test = MyTest('test_c')
-test.run()
+test.run(result)
+
+print(result.summary())
