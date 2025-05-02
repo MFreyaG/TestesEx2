d7e6869b323100e4ba33c7e7b12c5dcf099b6746
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2025-05-01 22:29:18 -0300

    5. Classe TestSuite

---
+++ framework_tests/test_suite_test.py
@@ -0,0 +1,35 @@
+from core.test_case import TestCase
+from core.test_result import TestResult
+from core.test_suite import TestSuite
+from .test_stub import TestStub
+    
+class TestSuiteTest(TestCase):
+
+    def test_suite_size(self):
+        suite = TestSuite()
+
+        suite.add_test(TestStub('test_success'))
+        suite.add_test(TestStub('test_failure'))
+        suite.add_test(TestStub('test_error'))
+
+        assert len(suite.tests) == 3
+
+    def test_suite_success_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub('test_success'))
+
+        suite.run(result)
+
+        assert result.summary() == '1 run, 0 failed, 0 error'
+
+    def test_suite_multiple_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub('test_success'))
+        suite.add_test(TestStub('test_failure'))
+        suite.add_test(TestStub('test_error'))
+
+        suite.run(result)
+
+        assert result.summary() == '3 run, 1 failed, 1 error'
