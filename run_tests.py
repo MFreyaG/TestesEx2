c17c2d1670da902c734e7c3d6660379be0b4d0e6
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2025-05-01 22:36:40 -0300

    7. Executando Todos os Testes

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,19 @@
 from core.test_loader import TestLoader
 from core.test_runner import TestRunner
+from core.test_suite import TestSuite
 from framework_tests.test_loader_test import TestLoaderTest
+from framework_tests.test_case_test import TestCaseTest
+from framework_tests.test_suite_test import TestSuiteTest
 
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
 
 runner = TestRunner()
 runner.run(suite)
