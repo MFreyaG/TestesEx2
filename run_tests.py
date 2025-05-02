6f46016092e600c4c12838197c4f498d9e7e9e16
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2025-05-01 22:34:50 -0300

    6. Classes TestLoader e TestRunner

--- run_tests.py
+++ run_tests.py
@@ -1,23 +1,9 @@
-from core.test_result import TestResult
-from core.test_suite import TestSuite
-from framework_tests.test_case_test import TestCaseTest
-from framework_tests.test_suite_test import TestSuiteTest
+from core.test_loader import TestLoader
+from core.test_runner import TestRunner
+from framework_tests.test_loader_test import TestLoaderTest
 
-result = TestResult()
-suite = TestSuite()
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
 
-suite.add_test(TestCaseTest('test_result_success_run'))
-suite.add_test(TestCaseTest('test_result_failure_run'))
-suite.add_test(TestCaseTest('test_result_error_run'))
-suite.add_test(TestCaseTest('test_result_multiple_run'))
-suite.add_test(TestCaseTest('test_was_set_up'))
-suite.add_test(TestCaseTest('test_was_run'))
-suite.add_test(TestCaseTest('test_was_tear_down'))
-suite.add_test(TestCaseTest('test_template_method'))
-
-suite.add_test(TestSuiteTest('test_suite_size'))
-suite.add_test(TestSuiteTest('test_suite_success_run'))
-suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
-suite.run(result)
-print(result.summary())
+runner = TestRunner()
+runner.run(suite)
